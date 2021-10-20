b78ac4d21f69df3a031008abcb23d05f36b2968e
Joseph Ongchangco <jongchangco@ggc.edu>
2021-10-20 01:06:18 -0400

    Skeleton code
    - setup for programming
    - skeleton for classes

---
+++ src/Game.java
@@ -0,0 +1,8 @@
+public class Game {
+	public GameController _unnamed_GameController_;
+
+	// todo: run main code here
+	public void main() {
+		throw new UnsupportedOperationException();
+	}
+}
---
+++ src/HealthConsumable.java
@@ -0,0 +1,11 @@
+public class HealthConsumable extends Item {
+	private int _healthValue;
+
+	public int getHealthValue() {
+		return this._healthValue;
+	}
+
+	public void setHealthValue(int aHealthValue) {
+		this._healthValue = aHealthValue;
+	}
+}
